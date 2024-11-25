52216abe70572f38cfa5beadbf86f6d3b7c0f3c6
Ongarj Thongchai <ongarj2314@hotmail.com>
2024-11-25 15:17:26 +0700

    15:17 อัพเดตโครงสร้าง

---
+++ business.py
@@ -0,0 +1,12 @@
+class FileControl():
+    def __init__(self) -> None:
+        pass
+    def adddata_to_textfile():
+        pass
+class Stock():
+    def __init__(self) -> None:
+        pass
+    def additem_stock(self,name: str,qty: int,price: int)->bool:
+        pass
+    def generateid(self)->int:
+        
--- main.py
+++ main.py
@@ -5,11 +5,22 @@ from business import *
 def main():
     print("Welcome to ALCOHOL SHOP")
     UI = UIControl()
-    userselect = UI.menustart()
-    match userselect:
-        case "BUY": UI.buyalcohol()
-        case "Manage Stock": UI.stock()
-        case "BUYlog": UI.buylog()
-        case "Exit": exit()
+    userselect = None
+    try:
+        while True:
+            userselect = UI.menustart()
+            match userselect:
+                case "BUY": 
+                    adult = UI.limitage()
+                    if adult:
+                        UI.buyalcohol()
+                    else:
+                        print("\033[31m Sorry:Staff can't sale Algohol\033[0m")
+                case "Manage Stock": UI.stock()
+                case "BUYlog": UI.buylog()
+                case "Exit": exit()
+    except(Exception):
+        print("ERROR: At Main() Func The Program's Restarting")
+        main()
 if __name__ == "__main__":
     main()
