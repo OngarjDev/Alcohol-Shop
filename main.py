ad9265925606133be98406bcaf2450632e9bdc49
Ongarj Thongchai <ongarj2314@hotmail.com>
2024-12-14 22:06:33 +0700

    22:06

--- main.py
+++ main.py
@@ -10,22 +10,22 @@ def main():
     UI = UIControl()
     userselect = None
     while True:
-        # try:
-        userselect = UI.menustart()
-        match userselect:
-            case "BUY": 
-                UI.select_alcohol()
-            case "Manage Stock": UI.stock()
-            case "BUYlog": UI.buylog()
-            case "Exit":
-                print("\033[92mExiting the program.\033[0m")
-                break  # ออกจากลูป while และโปรแกรม
-        # except ValueError:
-        #     print("\033[31mInvalid input. Please enter a valid option.\033[0m")
-        # except Exception as e:
-        #     print(f"ERROR: {e}")
-        #     print("Program will restart.\n")
-        #     continue  # ให้โปรแกรมดำเนินการต่อและรับคำสั่งใหม่
+        try:
+            userselect = UI.menustart()
+            match userselect:
+                case "BUY": 
+                    UI.select_alcohol()
+                case "Manage Stock": UI.stock()
+                case "BUYlog": UI.buylog()
+                case "Exit":
+                    print("\033[92mExiting the program.\033[0m")
+                    break  # ออกจากลูป while และโปรแกรม
+        except ValueError:
+            print("\033[31mInvalid input. Please enter a valid option.\033[0m")
+        except Exception as e:
+            print(f"ERROR: {e}")
+            print("Program will restart.\n")
+            continue  # ให้โปรแกรมดำเนินการต่อและรับคำสั่งใหม่
 
 if __name__ == "__main__":
     main()
